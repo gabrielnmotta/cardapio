d5e18df1782dfe6f0d127cef0003f39b763b8e0c
Gabriel Nogueira <gabrieln500@gmail.com>
2022-08-10 15:40:29 -0300

    third commit

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,11 +2,14 @@ import styles from "./Cardapio.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
 import Buscador from "./Buscador";
 import { useState } from "react";
-import Filtros from "pages/Filtros";
+import Filtros from "./Filtros";
+import Ordenador from "./Ordenador";
+import Itens from './Itens';
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
-  const [filtro, setFiltro] = useState<number | null>(null)
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState("");
   return (
     <main>
       <nav className={styles.menu}>
@@ -14,15 +17,17 @@ export default function Cardapio() {
       </nav>
       <header className={styles.header}>
         <div className={styles.header__text}>
-          A casa do código e da massa
+          Restaurante Dev
         </div>
       </header>
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
         <Buscador busca={busca} setBusca={setBusca} />
         <div className={styles.cardapio__filtros}>
-          <Filtros filtro={filtro} setFiltro={setFiltro}/>
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
         </div>
+        <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
       </section>
     </main>
   )
