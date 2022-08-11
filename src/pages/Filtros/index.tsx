f9c2d063e0335dd693720a605fdf602fa00567e4
Gabriel Nogueira <gabrieln500@gmail.com>
2022-08-11 15:28:33 -0300

    learning about react router

--- src/pages/Filtros/index.tsx
+++ src/pages/Filtros/index.tsx
@@ -1,6 +1,6 @@
 import filtros from './filtros.json';
-import styles from "./Filtros.module.scss";
-import classNames from 'classnames'
+import styles from './Filtros.module.scss';
+import classNames from 'classnames';
 
 type IOpcao = typeof filtros[0];
 
@@ -10,20 +10,20 @@ interface Props {
 }
 
 export default function Filtros({filtro, setFiltro}: Props){
-    function selecionarFiltro(opcao: IOpcao){
-        if(filtro === opcao.id) return setFiltro(null)
-        return setFiltro(opcao.id)
-    }
-    return (
+  function selecionarFiltro(opcao: IOpcao){
+    if(filtro === opcao.id) return setFiltro(null);
+    return setFiltro(opcao.id);
+  }
+  return (
     <div className={styles.filtros}>
-        {filtros.map((opcao: IOpcao) => (
-            <button className={classNames({
-                [styles.filtros__filtro]: true,
-                [styles["filtros__filtro--ativo"]]: filtro === opcao.id
-            })} key={opcao.id} onClick={() => selecionarFiltro(opcao)}>
-                {opcao.label}
-            </button>
-        ))}
+      {filtros.map((opcao: IOpcao) => (
+        <button className={classNames({
+          [styles.filtros__filtro]: true,
+          [styles['filtros__filtro--ativo']]: filtro === opcao.id
+        })} key={opcao.id} onClick={() => selecionarFiltro(opcao)}>
+          {opcao.label}
+        </button>
+      ))}
     </div>
-    )
+  );
 }
